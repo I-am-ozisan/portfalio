03bcb611ea4cfda5c42e83581059e4c1423c813a
taka <02070808wt@gmail.com>
2022-01-04 21:44:34 +0900

    【update】修正

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $(".nav-work").on("click", function() {
     $("html,body").animate({ scrollTop: $('.h1-work').offset().top });
 });
 
-$("a").on("click", function() {
+$(".siteLink").on("click", function() {
     event.preventDefault();
     window.open($(this).attr('href'), '_blank');
 })
